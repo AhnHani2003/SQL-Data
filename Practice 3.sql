ae350e5abd8d9e9703ed862aaeedc75b1b09c991
Trần Thông <145345242+AhnHani2003@users.noreply.github.com>
2024-03-19 11:25:04 +0700

    Create Practice 3.sql

---
+++ Practice 3.sql
@@ -0,0 +1,59 @@
+--ex1: Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name,3),ID 
+/*ex2: Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+Return the result table ordered by user_id.
+The result format is in the following example.*/
+SELECT user_id,
+CONCAT(UPPER(LEFT(name,1)), LOWER(RIGHT(name,LENGTH(name)-1))) AS name
+FROM Users
+ORDER BY user_id
+/*ex3: CVS Health wants to gain a clearer understanding of its pharmacy sales and the performance of various products.
+
+Write a query to calculate the total drug sales for each manufacturer. Round the answer to the nearest million and report your results in descending order of total sales. In case of any duplicates, sort them alphabetically by the manufacturer name.
+
+Since this data will be displayed on a dashboard viewed by business stakeholders, please format your results as follows: "$36 million".*/
+SELECT manufacturer,
+'$' || ROUND(SUM(total_sales)/1000000,0) || ' million' as sale
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER BY SUM(total_sales) DESC, manufacturer
+/*ex4: Given the reviews table, write a query to retrieve the average star rating for each product, grouped by month. The output should display the month as a numerical value, product ID, and average star rating rounded to two decimal places. Sort the output first by month and then by product ID.*/
+SELECT EXTRACT(MONTH FROM submit_date) as mth,
+product_id as product,
+ROUND(AVG(stars),2) as avg_stars
+FROM reviews
+GROUP BY EXTRACT(MONTH FROM submit_date), product_id
+ORDER BY mth, product
+--ex5: Write a query to identify the top 2 Power Users who sent the highest number of messages on Microsoft Teams in August 2022. Display the IDs of these 2 users along with the total number of messages they sent. Output the results in descending order based on the count of the messages.
+--No two users have sent the same number of messages in August 2022.
+SELECT sender_id,
+COUNT(message_id) as message_count
+FROM messages
+WHERE EXTRACT(MONTH FROM sent_date) = 8 AND EXTRACT(YEAR FROM sent_date)=2022
+GROUP BY sender_id
+ORDER BY message_count DESC
+LIMIT 2
+/*ex6: Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+Return the result table in any order.
+The result format is in the following example.*/
+SELECT tweet_id
+FROM Tweets
+WHERE LENGTH(content) >15
+/*ex7: Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
+Return the result table in any order.
+The result format is in the following example.*/
+/*ex8: You have been asked to find the number of employees hired between the months of January and July in the year 2022 inclusive.
+Your output should contain the number of employees hired in this given time frame.*/
+select count(id) as number_of_employees
+from employees
+where extract(month from joining_date) between 1 and 7
+and extract(year from joining_date) = 2022
+--ex9: Find the position of the lower case letter 'a' in the first name of the worker 'Amitah'.
+select position('a' in first_name)
+from worker
+where first_name='Amitah'
+--ex10: Find the vintage years of all wines from the country of Macedonia. The year can be found in the 'title' column. Output the wine (i.e., the 'title') along with the year. The year should be a numeric or int data type.
+
