eb615d6fd9827dabc57c14459fe325fade4b4697
Trần Thông <145345242+AhnHani2003@users.noreply.github.com>
2024-03-24 01:16:38 +0700

    Update practice 5.sql

--- practice 5.sql
+++ practice 5.sql
@@ -32,3 +32,40 @@ ON a.product_id = b.product_id
 WHERE b.product_name LIKE 'Azure%'
 GROUP BY customer_id
 HAVING COUNT(DISTINCT b.product_category) = (SELECT COUNT(DISTINCT product_category) FROM products)
+/*ex5: For this problem, we will consider a manager an employee who has at least 1 other employee reporting to them.
+Write a solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
+Return the result table ordered by employee_id.*/
+select mng.employee_id, mng.name, 
+count(emp.reports_to) as reports_count, 
+round(avg(emp.age),0) as average_age 
+from Employees mng 
+left join Employees emp on mng.employee_id=emp.reports_to 
+where emp.reports_to is not null
+group by mng.employee_id, mng.name
+order by mng.employee_id
+/*ex6: Write a solution to get the names of products that have at least 100 units ordered in February 2020 and their amount.
+Return the result table in any order.*/
+select a.product_name,
+sum(b.unit) as unit
+from Products a
+join Orders b
+on a.product_id = b.product_id  
+where b.order_date between '2020-02-01' and '2020-02-29'
+group by a.product_name
+having sum(b.unit) >=100
+/*ex7: Assume you're given two tables containing data about Facebook Pages and their respective likes (as in "Like a Facebook Page").
+Write a query to return the IDs of the Facebook pages that have zero likes. The output should be sorted in ascending order based on the page IDs.*/
+SELECT a.page_id
+FROM pages a
+LEFT JOIN page_likes b
+ON a.page_id = b.page_id
+WHERE b.liked_date IS NULL
+ORDER BY a.page_id
+--Question 1
+select 
+distinct min(replacement_cost)
+from film
+--Question 2
+
+
+
